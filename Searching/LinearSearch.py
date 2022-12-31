2cea243f5c50141fbc5306076cb8c81354676b47
Ayman Makroo <ajm8982@gmail.com>
2022-12-31 10:47:02 +0530

    Linear Search Updated

---
+++ Searching/LinearSearch.py
@@ -0,0 +1,14 @@
+def linear_search(num,target):
+    index = 0
+    for item in num:
+        if item == target:
+            return index;
+        index += 1
+
+
+nums = [1, 34, 88, 74, 3, 90, 22]
+target =  22
+print(linear_search(nums,target))
+
+
+
