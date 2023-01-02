b11a2949ade9a961dd4ea6dc250fe8be769a86a3
Ayman Makroo <ajm8982@gmail.com>
2023-01-02 20:45:01 +0530

    OrderAgnoisticBS Added

--- Searching/BinarySearch.py
+++ Searching/BinarySearch.py
@@ -11,6 +11,26 @@ def binarySearch(arr,target):
             return mid
     return -1
 def OrderAgnosticBS(arr,target):
-    
+    start = 0;
+    end = len(arr)-1
+    while(start <= end):
+        mid = int(start + (end - start)/2)
+        if arr[mid] == target:
+            return mid
+
+        if(arr[0]>arr[-1]):
+            if target > arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if target < arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+
+    return -1
+
+
 arr = [2, 3, 4, 16, 18, 21, 25, 40]
-print(binarySearch(arr,40))
+print(OrderAgnosticBS(arr[::-1],40))
