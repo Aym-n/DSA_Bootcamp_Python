059a9fbd17aa3b3e11219220967d89927e23ef5f
Ayman Makroo <ajm8982@gmail.com>
2023-01-02 20:36:46 +0530

    :)

--- Searching/tempCodeRunnerFile.py
+++ Searching/tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-def linear_search(num,target):
-    index = 0
-    for item in num:
-        if item == target:
-            return index;
-        index += 1
-
-
-nums = [1, 34, 88, 74, 3, 90, 22]
-target =  90
-print(linear_search(nums,target))
-
-
-
+target > arr[mid]
